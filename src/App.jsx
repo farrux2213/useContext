3106c6ca6748973c4e1a9670d29be655c19adfa9
farrux2213 <asqaraliyevfarrux2213@gmail.com>
2024-05-26 11:28:46 +0500

    created movies API with useContext

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Body from "./Body";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Body />
+      <Footer />
+    </>
+  );
+}
+export default App;
---
+++ src/Body/index.jsx
@@ -0,0 +1,11 @@
+import Card from "./Card";
+
+const Body = () => {
+  return (
+    <div>
+      <Card />
+    </div>
+  );
+};
+
+export default Body;
